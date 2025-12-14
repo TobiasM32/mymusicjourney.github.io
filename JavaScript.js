e19ae135c90ba4654298b911aa74c8137ff629bd
Tobias <tobias8@hawaii.edu>
2025-12-14 15:58:20 -0800

    Implement  JS interactivity

--- JavaScript.js
+++ JavaScript.js
@@ -4,9 +4,9 @@ const videos = document.getElementById("Favorite Songs");
 button.addEventListener("click", () => {
     if (videos.style.display === "none") {
         videos.style.display = "block";
-        button.textContent = "Hide my favorite songs";
+        button.textContent = "Show my favorite songs";
     } else {
         videos.style.display = "none";
-        button.textContent = "Click this button to see some of my favorite songs";
+        button.textContent = "Hide my favorite songs";
     }
 });
