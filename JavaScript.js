fc1603b7d0d6668cacb754a16b85371b5b3ac5a0
Tobias <tobias8@hawaii.edu>
2025-12-14 23:06:42 -0800

    Implement bootstrap

--- JavaScript.js
+++ JavaScript.js
@@ -1,14 +1,13 @@
 /* source: https://www.w3schools.am/howto/howto_js_toggle_hide_show.html#gsc.tab=0 */
 /* Makes the button show/hide the table of videos */
-function toggleSongs() {
+document.getElementById("toggleVideos").addEventListener("click", function() {
     const section = document.getElementById("favoriteSongs");
-    const currentDisplay = window.getComputedStyle(section).display;
-
-    if (currentDisplay === "none") {
+    if (window.getComputedStyle(section).display === "none") {
         section.style.display = "block";
-        document.getElementById("toggleButton").textContent = "Hide my favorite songs";
+        this.textContent = "Hide my favorite songs";
     } else {
         section.style.display = "none";
-        document.getElementById("toggleButton").textContent = "Show my favorite songs";
+        this.textContent = "Show my favorite songs";
     }
-}
+});
+
