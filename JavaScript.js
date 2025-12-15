c08c38e6693faf27857d2842573bc5edf03c9226
Tobias <tobias8@hawaii.edu>
2025-12-15 15:57:12 -0800

    touch up

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,14 @@
+// https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+// makes the button show/hide the table of videos
 const button = document.getElementById("toggleVideos");
 const section = document.getElementById("favoriteSongs");
 
 button.addEventListener("click", () => {
     if (window.getComputedStyle(section).display === "none") {
         section.style.display = "block";
-        button.textContent = "Hide my favorite songs";
+        button.textContent = "Favorite songs (show/hide)";
     } else {
         section.style.display = "none";
-        button.textContent = "Show my favorite songs";
+        button.textContent = "Favorite songs (show/hide)";
     }
 });
