d52c3884adb66e45f82dffa4dbf0966e2ecc59b1
Tobias <tobias8@hawaii.edu>
2025-12-17 00:04:08 -0800

    add more stuff

--- HomeVidButton.js
+++ HomeVidButton.js
@@ -1,3 +1,9 @@
+/*
+Student Name: Tobias McMillan
+File Name: HomeVidButton.js
+Date: 12/14/2025
+*/
+
 // https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 // makes the button show/hide the table of videos
 const button = document.getElementById("toggleVideos");
